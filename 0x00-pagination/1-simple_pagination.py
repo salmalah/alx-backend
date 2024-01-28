022bac517d7467cb8828c051c2a01f1e130e0aba
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-28 19:04:51 +0100

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -23,11 +23,13 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """
+        Initializes a new Server instance.
+        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """
-        define Cached dataset
+        """Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -38,13 +40,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data.
         """
-        Returns the specified page of the dataset
-        """
-        assert isinstance(page, int) and page > 0,
-        "Page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0,
-        "Page size must be a positive integer"
-
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
-        return self.dataset()[start:end]
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
