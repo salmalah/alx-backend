d279a4e152f5b6bf2bab5293d801bcc3eb9dd7a2
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-30 22:37:10 +0100

    Create 0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module is to define BasicCache module
+"""
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache inherits from BaseCaching and is a caching system without a limit.
+    """
+
+    def put(self, key, item):
+        """
+        Add an item in the cache without any limit
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get item by key
+        """
+        return self.cache_data.get(key) if key is not None else None
